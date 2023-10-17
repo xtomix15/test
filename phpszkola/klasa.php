eace0722101a8e7e510b4c226eadce639e368049
xtomix15 <xtomix13@gmail.com>
2023-10-17 10:39:23 +0200

    poprawka klasa

--- phpszkola/klasa.php
+++ phpszkola/klasa.php
@@ -12,4 +12,12 @@
             return $this->name;
         }
     }
+    $apple = new Fruit();
+    $banana = new Fruit();
+    $apple->set_name('Apple');
+    $banana->set_name('Banana');
+
+    echo $apple->get_name();
+    echo "<br>";
+    echo $banana->get_name();
 ?>
