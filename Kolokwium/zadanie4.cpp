794772226ad0c4f032a8ceb7cc93071bfbfeed63
IjaatTayHaai <93621864+IjaatTayHaai@users.noreply.github.com>
2026-01-21 13:19:35 +0100

    asdasda

--- Kolokwium/zadanie4.cpp
+++ Kolokwium/zadanie4.cpp
@@ -5,20 +5,28 @@
 using namespace std;
 
 void DecodeAndPrint(const string& coded) {
+
+    cout << "Wiadomosc zakodowana:" << endl;
     cout << coded << endl;
 
+    cout << "Wiadomosc zdekodowana:" << endl;
+    
     stringstream ss(coded);
     string token;
 
+ 
     while (ss >> token) {
         char type = token[0];
 
         if (type == 'L') {
+           
             int val = stoi(token.substr(1));
             cout << (char)('A' + val);
         } else if (type == 'N') {
+            
             cout << token.substr(1);
         } else if (type == 'X') {
+          
             cout << ' ';
         }
     }
